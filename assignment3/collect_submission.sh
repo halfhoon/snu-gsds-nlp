ec502fd97f6ee99788687e6e8cf733580f3e831b
Jaehoon Choi <158256936+jaehoonchoi95@users.noreply.github.com>
2024-10-22 14:03:44 +0900

    assignment 3 initialized

---
+++ assignment3/collect_submission.sh
@@ -0,0 +1,5 @@
+conda config --add channels conda-forge
+conda install zip
+rm -f 2000_00000_coding.zip #change here to your student id
+zip -r 2000_00000_coding.zip ./q3/*.py ./q3/model.bin ./q3/model.bin.optim ./q3/outputs/test_outputs*.txt
+#change above to your student id
